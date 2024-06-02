2a836df49e2294ef78026b59e0ba50ae0030f7da
leo_riger <leo.riger@edenred.com>
2024-06-02 14:59:59 +0200

    new dumps

--- migrations/Version20240602125728.php
+++ migrations/Version20240602125728.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240602125004 extends AbstractMigration
+final class Version20240602125728 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,13 +20,15 @@ final class Version20240602125004 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE hero ADD level VARCHAR(255) NOT NULL');
+        $this->addSql('CREATE SEQUENCE hero_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE hero (id INT NOT NULL, name VARCHAR(255) NOT NULL, level VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('ALTER TABLE hero DROP level');
+        $this->addSql('DROP SEQUENCE hero_id_seq CASCADE');
+        $this->addSql('DROP TABLE hero');
     }
 }
